7a00dac6afefb106e4728a1eb68d90709d8e0f9e
Denys Roshchin <denys.roshchin@gmail.com>
2016-11-21 14:22:39 +0200

    apptastico

--- apptastico/src/js/main.js
+++ apptastico/src/js/main.js
@@ -1,1 +1,16 @@
-console.log(3);
+$(function() {
+	$('.slick').slick({
+		dots: false,
+		slidesToShow: 4,
+		centerMode: true,
+		centerPadding: 60
+	});
+
+	 $('a.page-scroll').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
