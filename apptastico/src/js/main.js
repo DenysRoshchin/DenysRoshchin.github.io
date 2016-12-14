44bf046a684662c9d32154f4bbbf4460218f62d0
Denys Roshchin <denys.roshchin@gmail.com>
2016-12-14 11:29:40 +0200

    s-oil

--- apptastico/src/js/main.js
+++ apptastico/src/js/main.js
@@ -6,52 +6,25 @@ $(function() {
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+  });
 
 	$('.slick').slick({
 		dots: false,
 		slidesToShow: 4,
-		centerMode: true,
-		prevArrow: false,
-    nextArrow: false,
-		 responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 750,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        centerMode: true,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: 10,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+    infinite: true,
+    leftArrow: true,
+    rightArrow: true,
+
+      autoplay: true,
+  autoplaySpeed: 2000,
 	});
 
-	 $('a.page-scroll').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+	$('a.page-scroll').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+       event.preventDefault();
     });
 
 });
