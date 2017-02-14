92a0a4b295b5231a8f0201a0a4d776d71d992787
Denys Roshchin <denys.roshchin@gmail.com>
2017-02-14 23:23:41 +0200

    project

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -1,24 +1,25 @@
 /* Set the width of the side navigation to 250px */
 
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "460px";
-// }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "460px";
+}
 
 
- // Set the width of the side navigation to 0 
-// function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-// }
+ Set the width of the side navigation to 0 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 
 // WHY DIDNT WORK HOW TO MAKE TRIGGER WITH JQ?
 
 $(function() {
 	$("#chart").click(function(){
-		$("mySidenav").style.width = "460px";
+		$("#mySidenav").css('width', '460px');
+		console.log('s');
 	});
 	$("body").click(function() {
-		$("mySidenav").style.width = "0px";
+		$("#mySidenav").css('width', '0px');
 	});
 });
 
