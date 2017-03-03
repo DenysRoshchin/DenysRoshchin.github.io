fd93ea3468f6c8dd3f63e7108ab25aaf31907551
Denys Roshchin <denys.roshchin@gmail.com>
2017-03-03 10:27:29 +0200

    edit

--- RWD/creatively-flex/src/js/script.js
+++ RWD/creatively-flex/src/js/script.js
@@ -1,9 +1,40 @@
-$(function() {
-	$('.slider-top').slick({
+// $(function() {
+// 	$('.slider-top').slick({
 
-	});
+// 	});
 	
-	$('.slider-work').slick({
+// 	$('.slider-work').slick({
 
-	});
-});
+// 	});
+// });
+
+var randomInt = function () {
+    return Math.floor(Math.random() * (100 - 1) + 1);
+  };
+
+    var box1 = {
+      height: randomInt(),
+      width: randomInt(),
+      length: randomInt()
+    };
+
+    var box2 = {
+      height: randomInt(),
+      width: randomInt(),
+      length: randomInt()
+    };
+
+    var box3 = {
+      height: randomInt(),
+      width: randomInt(),
+      length: randomInt()
+    };
+
+    var volume = function(obj) {
+     	return this.height * this.width * this.length;
+    };
+
+    console.log(volume(box3));
+    console.log(volume(box2));
+    
+
