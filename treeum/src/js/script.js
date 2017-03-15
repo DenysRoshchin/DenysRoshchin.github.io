fa1dc975182e61abd6bda048f5238743cf5624ab
Denys Roshchin <denys.roshchin@gmail.com>
2017-03-15 19:53:59 +0200

    treeum

--- treeum/src/js/script.js
+++ treeum/src/js/script.js
@@ -1,6 +1,14 @@
 $(function() {
-	$('error-block__title').click(function() {
-		$(this).addClass('animated infinite bounce');
-		console.log('e');
+	$(".search-btn").click(function() {
+		$(".search-bar").addClass("visible animated slideInRight");	
+	});
+
+	$('.error-block__title').on("mouseenter", function() {
+		$(".error-block__title").addClass("animated bounceIn");
+	});
+
+	$('.footer-menu__links').click(function() {
+		$('.footer-menu__links').removeClass('footer-menu__links--active')
+		$(this).addClass('footer-menu__links footer-menu__links--active');
 	});
 })
