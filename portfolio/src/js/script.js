0eb897ed8af25adb9bc9dd53d66388fac8c077e0
Denys Roshchin <denys.roshchin@gmail.com>
2017-04-14 12:53:26 +0300

    update

--- portfolio/src/js/script.js
+++ portfolio/src/js/script.js
@@ -1,9 +1,14 @@
-$(function() {
-	$('.slider').slick({
-		// infinyte: true,
-		// autoplay: true
-		autoScaleSlider:false,
-		autoHeight: false
-	})
-})
 
+// $(function() {
+// 	$('.slider').slick({
+// 		// infinyte: true,
+// 		// autoplay: true
+// 		autoScaleSlider:false,
+// 		autoHeight: false
+// 	})
+// })
+
+var str = "The eagle has landed";
+console.log(str.split(' ')[1]);
+console.log(str.slice(4, 8));
+console.log(str.slice(4).slice(0, -11));
---
+++ project/src/js/script.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// function List(obj) {
+// 	this.$ = document.auerySlector("[data-component = list]");
+// 	this.items = document.querySelectorAll('li');
+
+// 	obj = {
+// 		$: this.$,
+// 		add(){
+// 			let string = 'Value ',
+// 			tag ="<li>"+ string +"</li>",
+// 			this.$.innerHTML += tag;			
+// 		},
+// 		delete(){
+// 			this.$.children[this.$.children.length-1].remove();
+// 		}
+// 	};
+
+// 	return obj;
+// };
+
+// (()=>{
+// 	let _List = new List();
+
+// })();
