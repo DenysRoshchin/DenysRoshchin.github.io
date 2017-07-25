79830bbdfa66753a065ee34e5acc6c535ca1efe1
Denys Roshchin <denys.roshchin@gmail.com>
2017-07-25 12:07:23 +0300

    update

--- dog-project/src/js/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.main-nav__btn').onclick = function() {
-	document.querySelector('.modal').style.display = 'block';
-};
-document.querySelector('.close-form').onclick =function() {
-	document.querySelector('.modal').style.display = 'none';
-};
